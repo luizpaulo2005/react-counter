9cdd2280e8503023b8b3c4e7f8c8ec51e85c1050
Luiz Paulo <luizpaulorodrigues2005@gmail.com>
2023-03-20 20:17:13 -0300

    button continue

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,32 @@ import { useEffect, useState } from "react";
 export const App = () => {
   const initial_time_in_seconds = 25 * 60; //25 Minutes
 
-  const [time, setTime] = useState(0);
+  const [time, setTime] = useState(initial_time_in_seconds);
+  const [isRunning, setIsRunning] = useState(false);
 
-  const minutes = Math.floor(time / 60);
-  const seconds = time % 60;
+  var hours = Math.floor(time / 3600)
+  var minutes = Math.floor(time / 60)
+  var seconds = time % 60;
 
-  const startTimer = () => {
-    setTime(initial_time_in_seconds);
+  if (hours >= 1){
+    minutes = minutes - 60 * hours
   }
 
   useEffect(() => {
-    time > 0 && setTimeout(() => setTime((state) => state - 1), 1000);
-  }, [time]);
+    isRunning && time > 0 && setTimeout(() => setTime((state) => state - 1), 1000);
+  }, [time, isRunning]);
 
   return (
     <div>
-      <button onClick={startTimer}>Start</button>
+      <button onClick={() => {setIsRunning(true); setTime(initial_time_in_seconds)}}>Start</button>
+      <button onClick={() => setIsRunning(false)}>Pause</button>
+      <button onClick={() => setIsRunning(true)}>Continuar</button>
+
+      <input type="number" disabled={isRunning} value={time} onChange={e => setTime(parseInt(e.target.value))}/>
       <br/>
+
+      <span>{hours.toString().padStart(2, "0")}</span>
+      <span>:</span>
       <span>{minutes.toString().padStart(2, "0")}</span>
       <span>:</span>
       <span>{seconds.toString().padStart(2, "0")}</span>
